0043b9feb4142060379120ed7f9971c77efd181d
shortsn <shortsn@users.noreply.github.com>
2016-03-04 10:22:29 +0100

    bar

--- TestApp/TestApp/StartupTask.cs
+++ TestApp/TestApp/StartupTask.cs
@@ -5,11 +5,14 @@ using System.Text;
 using System.Net.Http;
 using Windows.ApplicationModel.Background;
 using System.Threading.Tasks;
+using Windows.Devices.Spi;
+using Windows.Devices.Enumeration;
+using Windows.UI.Xaml;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Gpio;
 
-namespace TestApp
-{
-    public sealed class StartupTask : IBackgroundTask
-    {
+namespace TestApp {
+  public sealed class StartupTask : IBackgroundTask {
     //Setup address
     private const string I2C_CONTROLLER_NAME = "I2C1"; //use for RPI2
     private const byte DEVICE_I2C_ADDRESS = 0x3F; // 7-bit I2C address of the port expander
@@ -24,8 +27,20 @@ namespace TestApp
     private const byte D7 = 0x07;
     private const byte BL = 0x03;
 
-    public void Run(IBackgroundTaskInstance taskInstance)
-        {
+
+    private const int LED_PIN = 4; // Use pin 12 if you are using DragonBoard
+    private GpioPin ledPin;
+
+    private const string SPI_CONTROLLER_NAME = "SPI0";  /* Friendly name for Raspberry Pi 2 SPI controller          */
+    private const int SPI_CHIP_SELECT_LINE = 0;       /* Line 0 maps to physical pin number 24 on the Rpi2        */
+    private SpiDevice SpiADC;
+
+    private int adcValue;
+
+    public void Run(IBackgroundTaskInstance taskInstance) {
+
+      InitAll().Wait();
+
       // Here is I2C bus and Display itself initialized.
       //
       //  I2C bus is initialized by library constructor. There is also defined PCF8574 pins 
@@ -72,11 +87,80 @@ namespace TestApp
       int x = 0;
 
       while (true) {
-        Task.Delay(1000).Wait();
+        ReadADC();
+        LightLED();
+
+        Task.Delay(250).Wait();
         lcd.gotoxy(0, 1);
         lcd.prints(x++.ToString());
       }
-      
+    }
+
+    private async Task InitAll() {
+      InitGpio();         /* Initialize GPIO to toggle the LED                          */
+      await InitSPI();    /* Initialize the SPI bus for communicating with the ADC      */
+    }
+
+    private async Task InitSPI() {
+
+      var settings = new SpiConnectionSettings(SPI_CHIP_SELECT_LINE);
+      settings.ClockFrequency = 1000000;   /* 0.5MHz clock rate                                        */
+      settings.Mode = SpiMode.Mode0;      /* The ADC expects idle-low clock polarity so we use Mode0  */
+      settings.SharingMode = SpiSharingMode.Exclusive;
+
+
+      string spiAqs = SpiDevice.GetDeviceSelector(SPI_CONTROLLER_NAME);
+      var deviceInfo = await DeviceInformation.FindAllAsync(spiAqs);
+      SpiADC = await SpiDevice.FromIdAsync(deviceInfo[0].Id, settings);
+
+    }
+
+    private void InitGpio() {
+      var gpio = GpioController.GetDefault();
+
+      /* Show an error if there is no GPIO controller */
+      if (gpio == null) {
+        throw new Exception("There is no GPIO controller on this device");
+      }
+
+      ledPin = gpio.OpenPin(LED_PIN);
+
+      /* GPIO state is initially undefined, so we assign a default value before enabling as output */
+      ledPin.Write(GpioPinValue.High);
+      ledPin.SetDriveMode(GpioPinDriveMode.Output);
+    }
+
+    /* Turn on/off the LED depending on the potentiometer position    */
+    private void LightLED() {
+      int adcResolution = 1024;
+
+      /* Turn on LED if pot is rotated more halfway through its range */
+      if (adcValue > adcResolution / 2) {
+        ledPin.Write(GpioPinValue.Low);
+      }
+      /* Otherwise turn it off                                        */
+      else {
+        ledPin.Write(GpioPinValue.High);
+      }
+    }
+
+    public void ReadADC() {
+      byte[] readBuffer = new byte[3]; /* Buffer to hold read data*/
+      byte[] writeBuffer = new byte[3] { 0x01, 0x80, 0 };
+
+
+      SpiADC.TransferFullDuplex(writeBuffer, readBuffer); /* Read data from the ADC                           */
+      adcValue = convertToInt(readBuffer);
+
+      System.Diagnostics.Debug.WriteLine($"{ Math.Round(adcValue/102.4, 0, MidpointRounding.ToEven)}");
+    }
+
+    public int convertToInt([ReadOnlyArray] byte[] data) {
+      int result = 0;
+      result = data[1] & 0x03;
+      result <<= 8;
+      result += data[2];
+      return result;
     }
   }
 }
