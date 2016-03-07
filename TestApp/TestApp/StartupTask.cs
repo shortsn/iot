717be104737ec913eec46cef71cf3cf3dc23522e
shortsn <shortsn@users.noreply.github.com>
2016-03-07 16:17:17 +0100

    media fump

--- TestApp/TestApp/StartupTask.cs
+++ TestApp/TestApp/StartupTask.cs
@@ -2,6 +2,7 @@
 using Windows.ApplicationModel.Background;
 using System.Threading.Tasks;
 using Windows.Devices.Gpio;
+using Microsoft.Maker.Media.UniversalMediaEngine;
 
 namespace TestApp {
   public sealed class StartupTask : IBackgroundTask {
@@ -15,6 +16,13 @@ namespace TestApp {
       //InitGpio();
       deferral = taskInstance.GetDeferral();
 
+      var mediaengine = new MediaEngine();
+      var result = mediaengine.InitializeAsync().AsTask().Result;
+      
+      mediaengine.MediaStateChanged += Mediaengine_MediaStateChanged;
+
+      mediaengine.Play(@"http://mp3.planetradio.de/planetradio/hqlivestream.mp3");
+     
       using (var shift_register = SR_74HC595N.ConnectAsync().Result) {
         using (var mcp3008 = ADC_MCP3008.ConnectAsync().Result) {
           using (var display = DisplayI2C.ConnectAsync().Result) {
@@ -31,7 +39,7 @@ namespace TestApp {
             // 0x00 => 00000
             // 0x00 => 00000 
             display.CreateChar(0x00, new byte[] { 0x00, 0x00, 0x0A, 0x00, 0x11, 0x0E, 0x00, 0x00 });
-
+            
             //display.ClearScreen();
             //display.BacklightOff();
 
@@ -39,8 +47,8 @@ namespace TestApp {
 
             //Task.Delay(2000).Wait();
             //display.BacklightOn();
-            
-            
+
+
             var sequence = new byte[] { 0, 1, 3, 7, 15, 31 };
 
             var channel0 = 0;
@@ -48,7 +56,7 @@ namespace TestApp {
 
             var counter = 0;
 
-            while ((channel0 + channel1) < 20) {
+            while ((channel0 + channel1) < 110) {
               display.SetCursor(0, 0);
               channel0 = Convert.ToInt16(mcp3008.ReadValue(0) / 102.4);
               display.PrintString($"Channel 1:{ channel0 }  ");
@@ -64,9 +72,12 @@ namespace TestApp {
               shift_register.SendByte(sequence[index]);
 
               display.SetCursor(0, 1);
-              channel1 = Convert.ToInt16(mcp3008.ReadValue(1) / 102.4);
+              channel1 = Convert.ToInt16(mcp3008.ReadValue(1) / 10.24);
               display.PrintString($"Channel 2:{ channel1 }  ");
-              
+
+              var volume = channel1 / 100d;
+              mediaengine.Volume = volume;
+
               Task.Delay(250).Wait();
             }
 
@@ -75,7 +86,11 @@ namespace TestApp {
         }
       }
     }
-    
+
+    private void Mediaengine_MediaStateChanged(MediaState state) {
+      System.Diagnostics.Debug.WriteLine(state.ToString());
+    }
+
     private void InitGpio() {
       var gpio = GpioController.GetDefault();
 
